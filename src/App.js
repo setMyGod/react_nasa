ed253ccc4686b220aad152684cf3ab63bfdd9bb8
SetMyGod1488 <ivangritsina@gmail.com>
2022-01-19 22:27:07 +0200

    homework2

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React from 'react';
 
 import './App.css';
+import Users from "./components/Users/Users";
+import UserDetails from "./components/UserDetails/UserDetails";
+import Posts from "./components/Posts/Posts";
+import css from './App.module.css';
+
 
 const App = () => {
     return (
         <div>
+            <div className={css.wrap}>
+                <Users/>
+                <UserDetails/>
+            </div>
+            <Posts/>
 
         </div>
     );
